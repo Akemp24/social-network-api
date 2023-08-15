d1b732b2e2f9b7fec4504a459f9cfb6c8d6515a5
Asiak <asiakemp24@gmail.com>
2023-08-15 19:22:25 -0400

    created the user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,33 @@
 // Define the User Model
 // import required packages
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+});
+
 // have code for attaching friends to user?
 // Export code
