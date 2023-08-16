cbf2a9f6c9381c0c4c4770ccd436809afd102b69
Asiak <asiakemp24@gmail.com>
2023-08-16 17:24:02 -0400

    pseudocoded the thought-controller.js

---
+++ controller/thought-controller.js
@@ -0,0 +1,18 @@
+// import thought and user model
+const { Thought, User } = require('../models');
+
+// get all thoughts
+
+// get single thought
+
+// create a new thought
+
+// update a thought
+
+// delete a thought
+
+// create a reaction
+
+// delete a reaction
+
+// export controller
