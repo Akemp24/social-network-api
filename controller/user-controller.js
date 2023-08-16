3418a5d5f1f63b7949ea3f1cbc9acd56a92bbcbd
Asiak <asiakemp24@gmail.com>
2023-08-16 17:21:22 -0400

    pseudocoded the user-controller.js file

---
+++ controller/user-controller.js
@@ -0,0 +1,16 @@
+// import the user model
+const { User } = require('../models');
+
+//to get all users
+
+//to get a single user
+
+// create a new user
+
+// update a user
+
+// delete a user
+
+// add a friend to friendlist
+
+// remove a friend from friendlist
