f6fb4c02ed5893fdd8cae434c9a8bff6a4ba3366
Asiak <asiakemp24@gmail.com>
2023-08-20 15:54:55 -0400

    created routes file and difined

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,35 @@
-// require needed packages
 const router = require('express').Router();
-// define the user routes
-// define the thought routes
-// export the routes
+
+// Import user and thought controller functions
+const userController = require('../controllers/user-controller');
+const thoughtController = require('../controllers/thought-controller');
+
+// Define user routes
+router.route('/users')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router.route('/users/:userId')
+  .get(userController.getUserById)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
+
+router.route('/users/:userId/friends/:friendId')
+  .post(userController.addFriend)
+  .delete(userController.removeFriend);
+
+// Define thought routes
+router.route('/thoughts')
+  .get(thoughtController.getAllThoughts)
+  .post(thoughtController.createThought);
+
+router.route('/thoughts/:thoughtId')
+  .get(thoughtController.getThoughtById)
+  .put(thoughtController.updateThought)
+  .delete(thoughtController.deleteThought);
+
+router.route('/thoughts/:thoughtId/reactions')
+  .post(thoughtController.createReaction)
+  .delete(thoughtController.deleteReaction);
+
+module.exports = router;
